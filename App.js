2bcabe2d76e8c24d938106dfd64632502d964010
Andrew Murray <atmurray@bellsouth.net>
2020-06-28 09:14:16 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import WelcomeScreen from './app/assets/screens/WelcomeScreen';
+import ViewImageScreen from "./app/assets/screens/ViewImageScreen"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ViewImageScreen />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'orange',
+
   },
+
 });
+
